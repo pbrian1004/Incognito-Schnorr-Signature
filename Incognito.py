e52c3a34b6b5fb7d8d7295245e1632cf18e1388e
pbrian1004 <54018045+pbrian1004@users.noreply.github.com>
2024-04-13 13:20:44 +1000

    Incognito.py

--- Incognito.py
+++ Incognito.py
@@ -240,121 +240,22 @@ for ii in range (0, PK_num):
     if Verify("I am ", fake_PK, Incog) != 1:
         print ("Incognito failed")
 
-# basic sign without bulletproofs
-def Sign(PK, j, sigma):
-    z = sigma[0]
-    c = sigma[1]
-    universal_pk_string = list_to_string(PK)
-    R = V_z(z, PK[j], c)
-    b_array = [0] * len(PK)
-    b_array[j] = 1
-    a_array = [-1] * len(PK)
-    a_array[j] = 0
 
-    ### 1. Commit pkj
-    g0 = sha256((c + universal_pk_string).encode()).hexdigest()
-    g0 = int(g0, 16) * g
-    beta = secrets.randbelow(p)
-    Cpk = g0 * beta + PK[j]
-
-    ### 2. ZK Proof for z
-    rz = secrets.randbelow(p)
-    rb = secrets.randbelow(p)
-    c = int(c, 16)
-    Rz = g * rz + g0 * rb * c
-    cz = sha256((str(Rz) + str(Cpk)).encode()).hexdigest()
-    cz = int(cz, 16)
-    sz = (rz + cz * z) % p
-    s_beta = (rb + cz * beta) % p
-    pi_z = [Rz, sz, s_beta]
-
-    ### 3. ZK Proof for b
-    ##### generators h, (g1, ..., gn), and (h1, ..., hn) are all equals to g #####
-    g_array = [None] * len(PK)
-    h_array = [None] * len(PK)
-    sa = [None] * len(PK)
-    sb = [None] * len(PK)
-    for i in range (len(PK)):
-        new_curve2 = curve.P256
-        g_array[i] = new_curve2.G
-        new_curve3 = curve.P256
-        h_array[i] = new_curve3.G
-        sa[i] = secrets.randbelow(p)
-        sb[i] = secrets.randbelow(p)
-
-    alpha = secrets.randbelow(p)
-    rho = secrets.randbelow(p)
-    zeta = secrets.randbelow(p)
-    A = h * alpha + inner_products(g_array, b_array) + inner_products(h_array, a_array)
-    S = h * rho + inner_products(g_array, sb) + inner_products(h_array, sa)
-    Spk = g0 * zeta + inner_products(PK, sb)
-    y = int(sha256((str(A) + str(S) + str(Spk) + str(g0) + str(Cpk) + str(1)).encode()).hexdigest(), 16)
-    w = int(sha256((str(A) + str(S) + str(Spk) + str(g0) + str(Cpk) + str(2)).encode()).hexdigest(), 16)
-
-    yn = consecutive_powers(y, len(PK))
-    t0 = (w ** 2) - (w ** 3)
-    In = [1] * len(PK)
-    wn = [w*i for i in In]
-    w2n = [(w**2)*i for i in In]
-    tau1 = secrets.randbelow(p)
-    tau2 = secrets.randbelow(p)
-    t1 = inner_products(sb, vector_addition(hadamard_products(yn, vector_addition(a_array, wn)), w2n)) 
-    + inner_products(vector_minus(b_array, wn), hadamard_products(yn, sa))
-    t2 = inner_products(sb, hadamard_products(yn, sa))
-    T1 = g * t1 + h * tau1
-    T2 = g * t2 + h * tau2
-
-    x = int(sha256((str(T1) + str(T2) + str(y) + str(w)).encode()).hexdigest(), 16)
-    Lx = vector_addition(vector_minus(b_array, wn), [x*i for i in sb])
-    Rx = vector_addition(hadamard_products(yn, vector_addition([x*i for i in sa], vector_addition(a_array, wn))), w2n)
-    tx = t0 + t1 * x + t2 * (x**2)
-    tau_x = tau2 * (x**2) + tau1 * x
-    mu = alpha + rho * x
-    nu = beta + zeta * x
-    pi_b = [A, S, Spk, T1, T2, tau_x, mu, nu, tx, Lx, Rx]
-
-    return (R, Cpk, pi_z, pi_b)
-
-
-def Verify(m, PK, sigma):
-    R = sigma[0]
-    Cpk = sigma[1]
-    pi_z = sigma[2]
-    pi_b = sigma[3]
-    [Rz, sz, s_beta] = pi_z
-    [A, S, Spk, T1, T2, tau_x, mu, nu, tx, Lx, Rx] = pi_b
-    universal_pk_string = list_to_string(PK)    
-    my_string = m + pt_to_string(R)
-    c = sha256(my_string.encode()).hexdigest()
-    g0 = sha256((c + universal_pk_string).encode()).hexdigest()
-    g0 = int(g0, 16) * g
-    c = int(c, 16)
-    cz = sha256((str(Rz) + str(Cpk)).encode()).hexdigest()
-    cz = int(cz, 16)
-    
-    if g*sz + g0*s_beta*c != Rz + R*cz + Cpk*c*cz:
-        print('original pi_z verification failed')
-        return 0    
-
-    y = int(sha256((str(A) + str(S) + str(Spk) + str(g0) + str(Cpk) + str(1)).encode()).hexdigest(), 16)
-    w = int(sha256((str(A) + str(S) + str(Spk) + str(g0) + str(Cpk) + str(2)).encode()).hexdigest(), 16)
-    x = int(sha256((str(T1) + str(T2) + str(y) + str(w)).encode()).hexdigest(), 16)
-    if g*tx + h*tau_x != g*((w ** 2) - (w ** 3)) + T1*x + T2*(x**2):
-        print('original pi_b verification failed')
-        return 0 
-
-    return 1
-
-# testing
-PK_num = 20
-for ii in range (0, PK_num):
-    fake_PK = [None]* (PK_num)
-    for i in range (0, PK_num):
-    #     fill it with fake first, then change later
-        foo, fake_PK[i] = KeyGen()
-    ssk, ppk = KeyGen()
-    my_sk, fake_PK[ii] = ssk, ppk
+power_of_2 = 10
+PK_num = 2 ** power_of_2
+time_trail = 1
+fake_PK = [None]* (PK_num)
+for i in range (0, PK_num):
+#     fill it with fake first, then change later
+    foo, fake_PK[i] = KeyGen()
+ssk, ppk = KeyGen()
+for ii in range (time_trail):
+    start_time = time.time()
+    random_position = secrets.randbelow(PK_num)
+    my_sk, fake_PK[random_position] = ssk, ppk
     hh = Schnorr_SIGN("I am ", my_sk)
-    Incog = Sign(fake_PK, ii, hh)
+    Incog = Sign(fake_PK, random_position, hh)
     if Verify("I am ", fake_PK, Incog) != 1:
         print ("Incognito failed")
+        # break
+    print('total time elaspsed ', time.time() - start_time)
